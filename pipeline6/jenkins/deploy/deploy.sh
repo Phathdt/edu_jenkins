6c703d510faf65d72bca84ec0e07fd4e89356dd2
Phathdt <phathdt379@gmail.com>
2020-09-06 09:35:01 +0700

    add deploy step

--- pipeline6/jenkins/deploy/deploy.sh
+++ pipeline6/jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp  /tmp/.auth root@113.164.246.4:/tmp/.auth
-scp ./pipeline6/jenkins/deploy/publish root@113.164.246.4:/tmp/publish
-ssh root@113.164.246.4 "/tmp/publish"
+scp  /tmp/.auth slave:/tmp/.auth
+scp ./pipeline6/jenkins/deploy/publish slave:/tmp/publish
+ssh slave "/tmp/publish"
