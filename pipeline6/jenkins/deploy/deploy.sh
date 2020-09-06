678a7bf7a3b122d3e6ddd229d830a46d1bcb7a26
Phathdt <phathdt379@gmail.com>
2020-09-06 10:01:16 +0700

    add docker-compose

--- pipeline6/jenkins/deploy/deploy.sh
+++ pipeline6/jenkins/deploy/deploy.sh
@@ -6,4 +6,5 @@ echo $PASS >> /tmp/.auth
 
 scp  /tmp/.auth slave:/tmp/.auth
 scp ./pipeline6/jenkins/deploy/publish slave:/tmp/publish
+scp ./pipeline6/docker-compose.yml slave:~/maven/docker-compose.yml
 ssh slave "/tmp/publish"
